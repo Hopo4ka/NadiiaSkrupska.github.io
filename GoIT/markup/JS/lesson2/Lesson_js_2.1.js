ac6f7a8b795119bc64b334ca77217a42c4c76745
Nadine <alexian@list.ru>
2015-10-05 22:45:10 +0300

    Lesson_js_2.1: done

---
+++ GoIT/markup/JS/lesson2/Lesson_js_2.1.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Edward on 05.10.2015.
+ */
+
+var year;
+
+year = prompt('Какой сейчас год?', '');
+if (year == 2015) {
+    alert('Вы правы!');
+} else {
+    alert('С луны свалился?');
+}
