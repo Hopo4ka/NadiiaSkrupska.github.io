ef60e5137698a4c030b682394e08e1e419992dba
Nadine <alexian@list.ru>
2015-10-05 22:53:46 +0300

    Lesson_js_2.2: done

---
+++ GoIT/markup/JS/lesson2/Lesson_js_2.2.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Edward on 05.10.2015.
+ */
+
+var chislo;
+
+chislo = prompt('Введите любое целое число', '');
+if (chislo > 0) {
+    alert('1');
+} else if (chislo < 0) {
+    alert('-1');
+} else {
+    alert('0');
+}
