df2592cc58bf226229557709e73d79b495f590b4
Nadine <alexian@list.ru>
2015-10-06 12:12:11 +0300

    Lesson_js_2: done

---
+++ GoIT/markup/JS/lesson2/Task_js_2_codewars1.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Edward on 06.10.2015.
+ */
+
+var num;
+
+function numberToString(num) {
+    return num.toString();
+}
---
+++ GoIT/markup/JS/lesson2/Task_js_2_codewars2.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Edward on 06.10.2015.
+ */
+
+var stringToNumber = function (str) {
+    return Number(str);// put your code here
+    return null;
+}
