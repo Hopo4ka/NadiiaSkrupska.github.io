cc7c62fae54108cc04e40460e99a3002547c9cc4
Nadine <alexina@list.ru>
2015-10-12 01:20:19 +0300

    Task 3.3: done

---
+++ GoIT/markup/JS/lesson3/Task_js_3.3.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Nadine on 10/12/2015.
+ */
+var a = 1, b = 100, j = 3, k = 5;
+nextnum: for (i = a; i <= b; i++) {
+    if (i % j === 0) {
+        console.log(i + ' - Fizz');
+    } else if (i % k === 0) {
+            console.log(i + ' - Buzz');
+    } else {
+        console.log(i);
+    }
+}
