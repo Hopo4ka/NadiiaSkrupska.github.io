93b712b492da9372a71fc2ed6d19a8060a339c1e
Nadine <alexina@list.ru>
2015-10-12 21:19:23 +0300

    Task 3.5: done

---
+++ GoIT/markup/JS/lesson3/Task_js_3.5.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Nadine on 10/12/2015.
+ */
+function drawBoard(w, h) {
+    var str = '';
+    for (i=1; i<=h; i++) {
+        for (j=1; j<=w; j++) {
+            if (((i+j) % 2) == 1)
+            str += ' ';
+            else
+            str += '#';
+        }
+        str += '\r\n';
+    }
+    console.log(str);
+}
+drawBoard(8, 8);
