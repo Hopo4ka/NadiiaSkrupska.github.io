248e7be67c0a06778988781f3590a96eac096587
Nadine <alexina@list.ru>
2015-10-13 01:10:52 +0300

    Task 3 pres: done

---
+++ GoIT/markup/JS/lesson3/Task_js_3.1_pres.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Nadine on 10/13/2015.
+ */
+function sum(a, b) {
+    return var sum = a + b;
+}
+
+console.log(sum (2, 5))
---
+++ GoIT/markup/JS/lesson3/Task_js_3.2_pres.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Nadine on 10/13/2015.
+ */
+function min(a, b) {
+    if (a < b) {
+        return a;
+    } else {
+        return b;
+    }
+}
+
+console.log(min(2, 5));
+console.log(min(2, -1));
