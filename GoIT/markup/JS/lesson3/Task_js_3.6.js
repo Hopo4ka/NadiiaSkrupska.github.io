014ba47a66f2884f235b6c5e725a2d490ac604d7
Nadine <alexina@list.ru>
2015-10-13 02:17:13 +0300

    Task 3.6: done

--- GoIT/markup/JS/lesson3/Task_js_3.6.js
+++ GoIT/markup/JS/lesson3/Task_js_3.6.js
@@ -2,10 +2,11 @@
  * Created by Nadine on 10/12/2015.
  */
 function pow(x, n) {
+    var y = x;
     for (i=1; i<n; i++) {
-        x *= x;
+        y *= x;
     }
-    return x;
+    return y;
 }
 console.log(pow(3, 2));
 console.log(pow(1, 100));
