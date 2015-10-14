6bb352bc4f0ee8fbbca99054405ff92d63857621
Nadine <alexina@list.ru>
2015-10-15 00:54:10 +0300

    Task 4.3 : done

---
+++ GoIT/markup/JS/lesson4/Task_js_4.3.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Nadine on 10/15/2015.
+ */
+function strCut(str) {
+    if (str.length <= 20)
+        return str;
+    else return str.slice(0, 21);
+}
+console.log(strCut('Hey there! Nice to meet you!'));
+console.log(strCut('Hey there!'));
