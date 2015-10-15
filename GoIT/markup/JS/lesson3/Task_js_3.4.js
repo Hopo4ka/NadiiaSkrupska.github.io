278544add272c5706cb4bbddde9b2fa157ae6149
Nadine <alexina@list.ru>
2015-10-15 22:26:25 +0300

    added {} to if

--- GoIT/markup/JS/lesson3/Task_js_3.4.js
+++ GoIT/markup/JS/lesson3/Task_js_3.4.js
@@ -1,9 +1,13 @@
 var j = 3, k = 5,
     num = prompt('Enter a number bigger than 100');
+
 while (num <= 100) {
     num = prompt('Enter a number bigger than 100');
 }
+
 if (num % j === 0) {
-    if (num % k === 0) alert('FizzBuzz');
+    if (num % k === 0) {
+        alert('FizzBuzz');
+    }
 }
 
