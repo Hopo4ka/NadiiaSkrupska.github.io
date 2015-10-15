aba4e3f161ec8821e48946e0d1977cfec54e4c35
Nadine <alexina@list.ru>
2015-10-15 22:29:03 +0300

    added {} to if

--- GoIT/markup/JS/lesson4/Task_js_4.3.js
+++ GoIT/markup/JS/lesson4/Task_js_4.3.js
@@ -1,7 +1,9 @@
 function strCut(str) {
-    if (str.length <= 20)
+    if (str.length <= 20) {
         return str;
-    else return str.slice(0, 21);
+    } else {
+        return str.slice(0, 21);
+    }
 }
 console.log(strCut('Hey there! Nice to meet you!'));
 console.log(strCut('Hey there!'));
