ea61d121aed257bde298af4998c3204764884d39
Nadine <alexina@list.ru>
2015-10-15 22:27:20 +0300

    added {} to if

--- GoIT/markup/JS/lesson3/Task_js_3.5.js
+++ GoIT/markup/JS/lesson3/Task_js_3.5.js
@@ -2,10 +2,11 @@ function drawBoard(w, h) {
     var str = '';
     for (i=1; i<=h; i++) {
         for (j=1; j<=w; j++) {
-            if (((i+j) % 2) == 1)
-            str += ' ';
-            else
+            if (((i+j) % 2) == 1) {
+                str += ' ';
+            } else {
             str += '#';
+            }
         }
         str += '\r\n';
     }
