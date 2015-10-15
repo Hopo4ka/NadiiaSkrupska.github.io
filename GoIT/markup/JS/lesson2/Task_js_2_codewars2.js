227bf33c558b9fd7db1a2cf1aae5f6062a5bbdf7
Nadine <alexina@list.ru>
2015-10-15 22:52:54 +0300

    rewrote  the code

--- GoIT/markup/JS/lesson2/Task_js_2_codewars2.js
+++ GoIT/markup/JS/lesson2/Task_js_2_codewars2.js
@@ -1,4 +1,7 @@
 var stringToNumber = function (str) {
-    return Number(str);// put your code here
-    return null;
+    return Number(str);
 }
+
+console.log(stringToNumber("1234"));
+console.log(stringToNumber("605" ));
+console.log(stringToNumber("1405"));
