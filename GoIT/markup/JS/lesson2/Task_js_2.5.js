061d4e084dfe6d212bcaa79d7c5e9129d5d54fba
Nadine <alexina@list.ru>
2015-10-15 03:16:12 +0300

    made some small fixing

--- GoIT/markup/JS/lesson2/Task_js_2.5.js
+++ GoIT/markup/JS/lesson2/Task_js_2.5.js
@@ -1,10 +1,7 @@
-/**
- * Created by Edward on 06.10.2015.
- */
+var name = 'admin';
 
-var name = 'admin', text;
-
-text = (name == 'admin') ? 'Hi' :
+var text = (name == 'admin') ? 'Hi' :
     (name == 'manager') ? 'Hello' :
         (name == '') ? 'No login' : '';
+
 alert(text);
