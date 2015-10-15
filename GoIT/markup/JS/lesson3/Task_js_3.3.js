1ce52f37ef90ed2313801af8808e19a958491cc1
Nadine <alexina@list.ru>
2015-10-15 22:54:59 +0300

    removed "nextnum:"

--- GoIT/markup/JS/lesson3/Task_js_3.3.js
+++ GoIT/markup/JS/lesson3/Task_js_3.3.js
@@ -1,5 +1,5 @@
 var a = 1, b = 100, j = 3, k = 5;
-nextnum: for (i = a; i <= b; i++) {
+for (i = a; i <= b; i++) {
     if (i % j === 0) {
         console.log(i + ' - Fizz');
     } else if (i % k === 0) {
