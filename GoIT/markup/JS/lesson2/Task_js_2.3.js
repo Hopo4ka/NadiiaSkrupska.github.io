e5fed660163bb2494e425baeeb4ad331cd488adf
Nadine <alexina@list.ru>
2015-10-15 03:00:44 +0300

    fixed the value in prompt

--- GoIT/markup/JS/lesson2/Task_js_2.3.js
+++ GoIT/markup/JS/lesson2/Task_js_2.3.js
@@ -2,10 +2,10 @@
  * Created by Edward on 05.10.2015.
  */
 
-var login = prompt('Enter Your Login:', '');
+var login = prompt('Login:', '');
 
 if (login == 'admin') {
-    var password = prompt('Enter Your Password', '');
+    var password = prompt('Password', '');
     if (password == 'passw0rd') alert('Welcome home!');
     else if (password == null) alert('Canceled');
     else alert('Wrong password!');
