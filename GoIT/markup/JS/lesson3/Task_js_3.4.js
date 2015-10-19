3027d5fc6b3361476e2ebcc80b55df12a656bf54
Nadine <alexina@list.ru>
2015-10-19 21:06:03 +0300

    rewrote the code according to the task

--- GoIT/markup/JS/lesson3/Task_js_3.4.js
+++ GoIT/markup/JS/lesson3/Task_js_3.4.js
@@ -1,8 +1,16 @@
 var a = 1, b = 100, j = 3, k = 5;
 for (i = a; i <= b; i++) {
-    if (i % j === 0 & i % k === 0) {
-        console.log(i + 'FizzBuzz');
-    } else {
+    if (i % j === 0) {
+        if (i % k === 0) {
+            console.log(i + ' - FizzBuzz');
+        } else {
+            console.log(i + ' - Fizz');
+        }
+    }
+    else if (i % k === 0) {
+        console.log(i + ' - Buzz');
+    }
+    else {
         console.log(i);
     }
 }
