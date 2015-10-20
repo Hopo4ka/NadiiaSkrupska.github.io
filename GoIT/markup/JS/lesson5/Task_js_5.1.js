4ecdf5a9def208b8c854ada5a2ddae4e7c68645c
Nadine <alexina@list.ru>
2015-10-21 00:12:54 +0300

    Task_js_5.1: done

---
+++ GoIT/markup/JS/lesson5/Task_js_5.1.js
@@ -0,0 +1,21 @@
+// in order to deliver the rigth solution, I have changed conditions a little bit, as 99 was the last number
+var tasksCompleted = {
+    'Anna': 29,
+    'Serg': 99,
+    'Elena': 1,
+    'Anton': 35
+};
+
+function maxNum(obj) {
+    var counter = 0;
+    for (var item in obj) {
+        if (counter < obj[item]){
+            counter = obj[item];
+        } else {
+            continue;
+        }
+    }
+    return counter;
+}
+
+console.log(maxNum(tasksCompleted));
