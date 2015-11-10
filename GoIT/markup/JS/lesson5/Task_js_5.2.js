3fc38b159e095efcd123025a61fadf07bc1dffb0
Nadine <alexina@list.ru>
2015-11-10 03:40:11 +0200

    Task_js_5.2: done

--- GoIT/markup/JS/lesson5/Task_js_5.2.js
+++ GoIT/markup/JS/lesson5/Task_js_5.2.js
@@ -1,12 +1,13 @@
 var image = {
     width: 100,
     height: 400,
-    title: 'Cool image'
+    title: 'Cool image',
+    title1: '200'
 };
 
 function multiplyNumeric(image) {
     for (var key in image) {
-        if (isNaN(image[key])) {
+        if (typeof image[key] == 'string') {
             image[key] = image[key];
         } else {
             image[key] = image[key] * 2;
