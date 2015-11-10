ac84b349d1426e2102349d0da9f6f035d07fa481
Nadine <alexina@list.ru>
2015-11-10 03:40:42 +0200

    Task_js_6.2: done

---
+++ GoIT/markup/JS/lesson6/Task_js_6.2.js
@@ -0,0 +1,6 @@
+var arr = ['HTML', 'JavaScript', 'CSS'];
+
+arrSorted = arr.sort();
+
+console.log( arrSorted ); // CSS, HTML, JavaScript
+console.log( arr ); // HTML, JavaScript, CSS (??? ?????????)
