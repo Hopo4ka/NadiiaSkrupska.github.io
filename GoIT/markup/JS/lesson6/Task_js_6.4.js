6cb7bade6e0158051ad464e71f206cbe8e38511f
Nadine <alexina@list.ru>
2015-11-10 22:05:49 +0200

    Task_js_6.4: done

---
+++ GoIT/markup/JS/lesson6/Task_js_6.4.js
@@ -0,0 +1,11 @@
+var vasya = { name: '????', age: 23 };
+var masha = { name: '????', age: 18 };
+var vovochka = { name: '???????', age: 6 };
+
+var people = [ masha , vovochka, vasya ];
+
+function sortName(a, b) {
+    return a.age - b.age;
+}
+
+console.log(people.sort(sortName));
