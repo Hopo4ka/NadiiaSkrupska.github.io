4af87be2c551cd59c67db14b26a05003d01c02c6
Nadine <alexina@list.ru>
2015-11-12 04:45:25 +0200

    Task_js_6.5: done

---
+++ GoIT/markup/JS/lesson6/Task_js_6.5.js
@@ -0,0 +1,20 @@
+function isPal(str) {
+    var arr = [];
+
+    for (var i=0; i<str.length; i++) {
+        if (str[i] != " ")
+            arr.push(str[i].toLowerCase());
+        else
+            continue;
+    }
+    var trimmed = arr.join('');
+    arr.reverse();
+    var trimmed_reverse = arr.join('');
+    return (trimmed === trimmed_reverse) ? true: false;
+}
+
+console.log(isPal('Anna')); // true
+console.log(isPal('? ???? ????? ?? ???? ?????')); //true
+console.log(isPal('????')); //false
+console.log(isPal('12321')); //true
+console.log(isPal('123212')); //false
