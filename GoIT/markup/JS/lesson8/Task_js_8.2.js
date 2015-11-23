5d2cbabfe2d29c94cbba13b0b0d785d2952db401
Nadine <alexina@list.ru>
2015-11-22 23:54:00 -0400

    Task_js_8.2: done

---
+++ GoIT/markup/JS/lesson8/Task_js_8.2.js
@@ -0,0 +1,30 @@
+function Calculator(){
+
+    var func_arr = {
+        "+" : function(a,b){return a+b;},
+        "-" : function(a,b){return a-b;}
+    };
+
+    this.calculate = function(value) {
+        var elem = value.split(' ');
+        return func_arr[ elem[1] ](+elem[0], +elem[2]);
+    };
+
+    this.addMethod = function(name, func){
+        func_arr[name] = func;
+    };
+
+}
+
+var calc = new Calculator();
+
+calc.addMethod("*", function(a, b) {
+    return a * b;
+});
+calc.addMethod("pov", function(a, b) {
+    return Math.pow(a,b);
+});
+console.log( calc.calculate("3 * 7") );
+console.log( calc.calculate("2 pov 2") );
+console.log( calc.calculate("3 + 7") ); // 10
+console.log( calc.calculate("3 - 7") ); 
