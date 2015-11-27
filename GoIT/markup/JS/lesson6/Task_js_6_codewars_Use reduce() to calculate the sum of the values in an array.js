4f69e5c9b25ad37a9bc7ee0cfbe05ee47c1fd2d8
Nadine <alexina@list.ru>
2015-11-27 10:46:54 -0400

    fixed

--- GoIT/markup/JS/lesson6/Task_js_6_codewars_Use reduce() to calculate the sum of the values in an array.js
+++ GoIT/markup/JS/lesson6/Task_js_6_codewars_Use reduce() to calculate the sum of the values in an array.js
@@ -1,6 +1,5 @@
 function sum(array) {
-    var result = array.reduce(function (sum, current) {
+    return array.reduce(function (sum, current) {
         return sum + current;
     });
-    return result;
 }
