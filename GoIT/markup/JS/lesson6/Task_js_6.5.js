5b54787b93c0f86c3f4b2a3d970d0e26cf3260fa
Nadine <alexina@list.ru>
2015-11-27 01:19:56 -0400

    fixed mistakes

--- GoIT/markup/JS/lesson6/Task_js_6.5.js
+++ GoIT/markup/JS/lesson6/Task_js_6.5.js
@@ -2,15 +2,14 @@ function isPal(str) {
     var arr = [];
 
     for (var i = 0; i < str.length; i++) {
-        if (str[i] != " ")
+        if (str[i] != " "){
             arr.push(str[i].toLowerCase());
-        else
-            continue;
+        }
     }
     var trimmed = arr.join('');
     arr.reverse();
     var trimmed_reverse = arr.join('');
-    return (trimmed === trimmed_reverse) ? true : false;
+    return (trimmed == trimmed_reverse);
 }
 
 console.log(isPal('Anna')); // true
