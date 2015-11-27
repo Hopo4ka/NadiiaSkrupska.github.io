a9249fd7562a3e202311663a97bde1b57343eb55
Nadine <alexina@list.ru>
2015-11-27 10:42:22 -0400

    fixed

--- GoIT/markup/JS/lesson6/Task_js_6_codewars_Return the Missing Element.js
+++ GoIT/markup/JS/lesson6/Task_js_6_codewars_Return the Missing Element.js
@@ -2,7 +2,7 @@ var missingArr = ['1', '3', '2', '5', '4', '9', '7', '6', '0'];
 
 function anClean(arr) {
     arr.sort();
-    var t = new Array();
+    var t = [];
     for (var i = 0; i <= 9; i++)
         if (arr.indexOf(i.toString()) == -1)
             t.push(i);
