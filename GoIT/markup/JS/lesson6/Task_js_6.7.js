1b627e42a826154a147829caeb41963634c46b50
Nadine <NadiiaSkrupska@users.noreply.github.com>
2015-11-27 09:34:58 -0400

    Update Task_js_6.7.js
    fixed

--- GoIT/markup/JS/lesson6/Task_js_6.7.js
+++ GoIT/markup/JS/lesson6/Task_js_6.7.js
@@ -1,10 +1,11 @@
-var string = ['???', '??????', '??????', '???', '??????', '??????', '??????'];
+var string = ['воз', 'киборг', 'корсет', 'ЗОВ', 'гробик', 'костер', 'сектор'];
 
 function unique(value, index, arr) {
     var t = [];
-    for (var k in arr)
-        t[k] = arr[k].toLowerCase().split("").sort().join("");
-    var edited = value.toLowerCase().split("").sort().join("");
+    for (i = 0; i < arr.length; i++) {
+        t[i] = arr[i].toLowerCase().split("").sort().join("");
+        var edited = value.toLowerCase().split("").sort().join("");
+    }
 
     return t.indexOf(edited) == index;
 }
@@ -13,4 +14,4 @@ function anClean(arr) {
     return arr.filter(unique);
 }
 
-console.log(anClean(string)); // '???,??????,??????' ??? '???,??????,??????'
+console.log(anClean(string)); 
