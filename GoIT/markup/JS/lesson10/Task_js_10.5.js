ef3299270793dba2688639d86dafc603ff17e841
Nadine <alexina@list.ru>
2015-11-30 23:33:56 -0400

    Task_js_10.5: done

--- GoIT/markup/JS/lesson10/Task_js_10.5.js
+++ GoIT/markup/JS/lesson10/Task_js_10.5.js
@@ -1,3 +1,8 @@
+/*Write a JavaScript function that accepts a string as a parameter
+and find the longest word within the string. Go to the editor
+ Example string: Web Development Tutorial and something else
+ Expected Output: Development*/
+
 function makeOrder(str) {
     var t = [];
 
