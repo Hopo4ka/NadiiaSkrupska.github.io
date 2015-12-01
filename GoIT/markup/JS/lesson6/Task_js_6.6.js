c3e0dc91d77a01f0ff0c649e275dcdfb12e5293e
Nadine <alexina@list.ru>
2015-11-30 23:55:58 -0400

    small fixing

--- GoIT/markup/JS/lesson6/Task_js_6.6.js
+++ GoIT/markup/JS/lesson6/Task_js_6.6.js
@@ -1,7 +1,10 @@
-var strings = ['??????', '??????', '????', '????', '????', '????', '??????', '??????', '8-()'];
+/*Напишите функцию unique(arr), которая возвращает массив,
+содержащий только уникальные элементы arr (arr — массив строк).*/
+
+var strings = ['кришна', 'кришна', 'харе', 'харе', 'харе', 'харе', 'кришна', 'кришна', '8-()' ];
 
 function unique(value, index, arr) {
     return arr.indexOf(value) == index;
 }
 
-console.log(strings.filter(unique)); // ??????, ????, 8-()
+console.log(strings.filter(unique)); // кришна, харе, 8-()
