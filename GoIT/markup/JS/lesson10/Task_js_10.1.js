67c46ac97e64c3099dfe388619b8a21718e4fdfe
Nadine <alexina@list.ru>
2015-11-30 22:41:16 -0400

    Task_js_10.1: done

---
+++ GoIT/markup/JS/lesson10/Task_js_10.1.js
@@ -0,0 +1,31 @@
+var obj = {
+    person1Age: 20,
+    person1Name: 'Ivanov',
+    person2Age: 30,
+    person2Name: 'Petrov',
+    person3Age: 40,
+    person3Name: 'Sidorov'
+};
+
+function extractString(obj) {
+    var names = [];
+    for (var key in obj) {
+        if (typeof obj[key] == 'string') {
+            names.push(obj[key]);
+        }
+    }
+    return names;
+}
+
+function extractNumber(obj) {
+    var ages = [];
+    for (var key in obj) {
+        if (typeof obj[key] != 'string') {
+            ages.push(obj[key]);
+        }
+    }
+    return ages;
+}
+
+console.log(extractNumber(obj));
+console.log(extractString(obj));
