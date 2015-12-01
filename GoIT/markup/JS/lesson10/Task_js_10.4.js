28884460f6324b8816a0ce1310753bbd0b7347b1
Nadine <alexina@list.ru>
2015-11-30 23:31:51 -0400

    Task_js_10.4: done

--- GoIT/markup/JS/lesson10/Task_js_10.4.js
+++ GoIT/markup/JS/lesson10/Task_js_10.4.js
@@ -1,3 +1,9 @@
+/*Write a JavaScript function that accepts a string as a parameter
+and converts the first letter of each word of the string in upper case.
+Go to the editor
+ Example string: the quick brown fox
+ Expected Output: The Quick Brown Fox*/
+
 function makeOrder(str) {
     var t = [];
 
