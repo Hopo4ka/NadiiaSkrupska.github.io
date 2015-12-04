65ae6336cb64507a30f5082d8369b5fcb30b5fe4
Nadine <alexina@list.ru>
2015-12-03 21:47:17 -0400

    Task_js_10_codewars7q_5: done

---
+++ GoIT/markup/JS/lesson10/Task_js_10_codewars7q_5.js
@@ -0,0 +1,12 @@
+/*Given an array, find the duplicates in that array, and return a
+ new array of those duplicates.
+ Note: numbers and their corresponding string representations
+ should not be treated as duplicates (i.e., '1' !== 1). */
+
+var arr = ['s', 'r', 3, 3, '3', 'y', '3', 't', 's', 3, 't', 't'];
+
+function duplicates(value, index, arr) {
+    return arr.indexOf(value) !== index;
+}
+
+console.log(arr.filter(duplicates));
