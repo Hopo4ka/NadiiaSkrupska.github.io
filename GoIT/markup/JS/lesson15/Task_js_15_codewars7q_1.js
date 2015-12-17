55a04c171be78644f98de745c0edad3c7974e8c2
Nadine <alexina@list.ru>
2015-12-17 15:38:15 -0400

    Task_js_15_codewars7q_1: done

---
+++ GoIT/markup/JS/lesson15/Task_js_15_codewars7q_1.js
@@ -0,0 +1,10 @@
+/*Javascript lets you re-open classes so you can add new functionality to
+existing classes and objects.
+ In this kata, you'll have to add a new method in the String class that calls the
+ upcase method (toUpperCase()), so that:
+ "abc".myNewMethod();
+ returns
+ "ABC" */
+
+
+String.prototype.myNewMethod = String.prototype.toUpperCase();
