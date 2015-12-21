fbb134dad4cc097c2dd3908090d74f5c384b65eb
Nadine <alexina@list.ru>
2015-12-21 16:28:04 -0400

    Task_js_16.4.3: done

---
+++ GoIT/markup/JS/lesson16/Task_js_16.4.3.js
@@ -0,0 +1,24 @@
+Если
+elem
+—
+это
+произвольный
+узел
+DOM
+…
+Верно
+ли, что
+elem.lastChild.nextSibling
+всегда
+null ? -Yes
+    Верно
+ли, что
+elem.children[0].previousSibling
+всегда
+null ? -No,
+    there
+can
+be
+some
+text
+
