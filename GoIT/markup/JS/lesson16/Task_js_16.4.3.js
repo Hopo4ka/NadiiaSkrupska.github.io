197f8abab1556709db820eaf426fbd6e08ceb96a
Nadine <alexina@list.ru>
2015-12-21 16:30:23 -0400

    Task_js_16.4.3: done

--- GoIT/markup/JS/lesson16/Task_js_16.4.3.js
+++ GoIT/markup/JS/lesson16/Task_js_16.4.3.js
@@ -1,21 +1,7 @@
-Если
-elem
-—
-это
-произвольный
-узел
-DOM
-…
-Верно
-ли, что
-elem.lastChild.nextSibling
-всегда
-null ? -Yes
-    Верно
-ли, что
-elem.children[0].previousSibling
-всегда
-null ? -No,
+/*Если elem — это произвольный узел DOM…
+ Верно ли, что elem.lastChild.nextSibling всегда null?*/
+-Yes
+    /*Верно ли, что elem.children[0].previousSibling всегда null ?*/ - No,
     there
 can
 be
