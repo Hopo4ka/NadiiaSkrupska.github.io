aa9a7de7993fe013bb45ce6c297cb16ea9ac2942
Nadine <alexina@list.ru>
2015-12-21 15:34:56 -0400

    Task_js_16.4.1:  in progress

---
+++ GoIT/markup/JS/lesson16/Task_js_16.4.1.js
@@ -0,0 +1,28 @@
+/* Для страницы:
+
+ <!DOCTYPE HTML>
+ <html>
+ <head>
+ <meta charset="utf-8">
+ </head>
+ <body>
+ <div>Пользователи:</div>
+ <ul>
+ <li>Маша</li>
+ <li>Вовочка</li>
+ </ul>
+ <!-- комментарий -->
+ <script>
+ // ... ваш код
+ </script>
+ </body>
+ </html>
+
+ Напишите код, который получит элемент HEAD.
+ Напишите код, который получит UL.
+ Напишите код, который получит второй LI. Будет ли ваш код работать в IE8-, если комментарий переместить между элементами LI?
+ */
+
+console.log(document.documentElement.firstChild);
+console.log(document.body.children[1]);
+console.log(document.body.children[1].children[1]);
