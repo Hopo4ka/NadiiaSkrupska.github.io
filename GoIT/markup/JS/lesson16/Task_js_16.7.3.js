c363ba06761acce25e968ff5c03d3f3d418355aa
Nadine <alexina@list.ru>
2015-12-22 14:28:24 -0400

    Task_js_16.7.3: done

---
+++ GoIT/markup/JS/lesson16/Task_js_16.7.3.js
@@ -0,0 +1,9 @@
+/* Что выведет этот код?
+
+ <script>
+ var body = document.body;
+ body.innerHTML = "<!--" + body.tagName + "-->";
+ alert( body.firstChild.data ); // что выведет?
+ </script>*/
+
+body
