62a76fbda5f84dcbf48f402aa4bcf177556888cd
Nadine <alexina@list.ru>
2015-12-22 14:20:06 -0400

    Task_js_16.7.2: done

---
+++ GoIT/markup/JS/lesson16/Task_js_16.7.2.js
@@ -0,0 +1,12 @@
+/* Что выведет скрипт на этой странице?
+
+ <!DOCTYPE HTML>
+ <html>
+   <body>
+      <script>
+      alert( document.body.lastChild.nodeType );
+      </script>
+   </body>
+ </html>*/
+
+1
