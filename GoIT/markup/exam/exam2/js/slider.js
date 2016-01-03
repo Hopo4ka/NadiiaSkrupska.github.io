abef9e570feebec59fcda44dde5ab8bfc7deee16
Nadine <alexina@list.ru>
2016-01-03 19:09:55 -0400

    exam

--- GoIT/markup/exam/exam2/js/slider.js
+++ GoIT/markup/exam/exam2/js/slider.js
@@ -7,36 +7,39 @@ function Slider(selector, options) {
         prevSliderNode = sliderNode.querySelector('.slider__pager_previous'),
         nextSliderNode = sliderNode.querySelector('.slider__pager_next');
 
-    var currentSlideIndex = options.currentSlide || 0,
-        itemsCount = sliderItemsNode.children.length,
-        slideSize = sliderItemsNode.offsetWidth + sliderItemsNode.offsetMarginLeft + sliderItemsNode.offsetMarginRight;
+    var currentSlideIndex = options.currentSlide || 0, //explain
+        itemsCount = sliderItemsNode.children.length - 1,
+        slideSize = sliderItemsNode.offsetWidth;
 
-    this.prevSlide = function() {
+    this.prevSlide = function () {
         if (currentSlideIndex === 0) {
-            currentSlideIndex = itemsCount - 1;
+            currentSlideIndex = itemsCount;
             return;
         }
         currentSlideIndex--;
     };
 
-    this.nextSlide = function() {
-        if (currentSlideIndex === itemsCount - 1) {
+    this.nextSlide = function () {
+        if (currentSlideIndex === itemsCount) {
             currentSlideIndex = 0;
             return;
         }
         currentSlideIndex++;
     };
 
-    this.__render = function() {
-        sliderItemsNode.style.marginleft = -(currentSlideIndex * slideSize) + 'px';
+    this.__render = function () {
+        sliderItemsNode.style.marginLeft = -(currentSlideIndex * slideSize) + 'px';
+        //explain
     };
 
-    prevSliderNode.onclick = function() {
+    prevSliderNode.onclick = function (e) {
+        e.preventDefault();
         __self.prevSlide();
         __self.__render();
     };
 
-    nextSliderNode.onclick = function() {
+    nextSliderNode.onclick = function (e) {
+        e.preventDefault();
         __self.nextSlide();
         __self.__render();
     };
